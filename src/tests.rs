fcaabcc5fc5d47650daaee700ffb0dd534c59d98
Mikhail <sadsnake@mixbytes.io>
2020-03-04 16:34:45 +0300

    Refactor `update_accounts` test

--- src/tests.rs
+++ src/tests.rs
@@ -1,21 +1,39 @@
 // Tests to be written here
 
 use crate::{mock::*, Error};
+use frame_support::dispatch;
 use frame_support::{assert_noop, assert_ok};
 
+fn create_oracle(source_limit: u8) -> dispatch::DispatchResult
+{
+    OracleModule::create_oracle(
+        Origin::signed(ALICE),
+        to_raw(ORACLE_NAME),
+        source_limit,
+        CALCULATION_PERIOD,
+        AGGREGATION_PERIOD,
+        ASSET_ID,
+        get_asset_names(),
+    )
+}
+
+fn self_votes(table_id: TableId, accounts_votes: Vec<(AccountId, Balance)>)
+{
+    accounts_votes.into_iter().for_each(|(account, balance)| {
+        assert_ok!(TablescoreModule::vote(
+            Origin::signed(account),
+            table_id,
+            balance,
+            account
+        ));
+    });
+}
+
 #[test]
-fn create_oracle()
+fn create()
 {
     new_test_ext().execute_with(|| {
-        assert_ok!(OracleModule::create_oracle(
-            Origin::signed(ALICE),
-            to_raw(ORACLE_NAME),
-            3,
-            CALCULATION_PERIOD,
-            AGGREGATION_PERIOD,
-            ASSET_ID,
-            get_asset_names()
-        ));
+        assert_ok!(create_oracle(4));
     });
 }
 
@@ -25,34 +43,21 @@ fn update_accounts()
     new_test_ext().execute_with(|| {
         let oracle_id = OracleModule::next_oracle_id();
         let table_id = TablescoreModule::next_table_id();
-
-        assert_ok!(OracleModule::create_oracle(
-            Origin::signed(ALICE),
-            to_raw(ORACLE_NAME),
-            3,
-            CALCULATION_PERIOD,
-            AGGREGATION_PERIOD,
-            ASSET_ID,
-            get_asset_names()
-        ));
-
-        let self_vote = |account, balance| {
-            assert_ok!(TablescoreModule::vote(
-                Origin::signed(account),
-                table_id,
-                balance,
-                account
-            ));
-        };
-
-        self_vote(ALICE, 96);
-        self_vote(OSCAR, 97);
-        self_vote(JUDY, 98);
-        self_vote(CAROL, 99);
-        self_vote(BOB, 100);
-        self_vote(EVE, 101);
+        assert_ok!(create_oracle(3));
 
         TimestampModule::set_timestamp(100);
+
+        self_votes(
+            table_id,
+            vec![
+                (ALICE, 96),
+                (OSCAR, 97),
+                (JUDY, 98),
+                (CAROL, 99),
+                (BOB, 100),
+                (EVE, 101),
+            ],
+        );
 
         let push = |account, moment, offset| {
             OracleModule::push(
@@ -66,9 +71,24 @@ fn update_accounts()
         assert_ok!(push(BOB, 0, 10));
         assert_ok!(push(CAROL, 0, 20));
 
-        assert_noop!(push(JUDY, 0, 20), Error::<Test>::AccountPermissionDenied);
-        assert_noop!(push(OSCAR, 0, 20), Error::<Test>::AccountPermissionDenied);
-        assert_noop!(push(ALICE, 0, 20), Error::<Test>::AccountPermissionDenied);
-        assert_noop!(push(ERIN, 0, 20), Error::<Test>::AccountPermissionDenied);
+        let denide = || Error::<Test>::AccountPermissionDenied;
+
+        assert_noop!(push(JUDY, 0, 20),  denide());
+        assert_noop!(push(OSCAR, 0, 20), denide());
+        assert_noop!(push(ALICE, 0, 20), denide());
+        assert_noop!(push(ERIN, 0, 20),  denide());
+
+        self_votes(table_id, vec![(ALICE, 100), (OSCAR, 100), (JUDY, 100)]);
+
+        TimestampModule::set_timestamp(700);
+
+        assert_ok!(push(ALICE, 0, 0));
+        assert_ok!(push(OSCAR, 0, 10));
+        assert_ok!(push(JUDY, 0, 20));
+
+        assert_noop!(push(EVE, 0, 20), denide());
+        assert_noop!(push(BOB, 0, 20), denide());
+        assert_noop!(push(CAROL, 0, 20), denide());
+        assert_noop!(push(ERIN, 0, 20),  denide());
     });
 }
